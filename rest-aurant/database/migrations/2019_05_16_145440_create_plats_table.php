46d5d9d24497920e31e5bf86dd8f02e7e490d8d4
Akamis013 <alex-v-pg@hotmail.fr>
2019-05-16 18:28:10 +0200

    Add some Models

---
+++ rest-aurant/database/migrations/2019_05_16_145440_create_plats_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plats', function (Blueprint $table) {
+            $table->bigIncrements('ID_plats');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plats');
+    }
+}
